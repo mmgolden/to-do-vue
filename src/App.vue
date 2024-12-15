cbdce016a38ddef960a6f6c1359677216801b1af
Melinda Golden <info@melindagolden.com>
2024-12-15 12:08:55 -0500

    feat: add base button component

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,16 @@
   <main class="mx-auto max-w-7xl p-4 sm:p-6 lg:p-8">
     <div class="mx-auto max-w-3xl">
       <BaseCard>
+        <template #header>
+          <div class="-ml-4 -mt-2 flex flex-wrap items-center justify-between sm:flex-nowrap">
+            <div class="ml-4 mt-2">
+              <h3 class="text-base font-semibold text-gray-900">Task List</h3>
+            </div>
+            <div class="ml-4 mt-2 shrink-0">
+              <BaseButton @click="startCreateTask"> New Task </BaseButton>
+            </div>
+          </div>
+        </template>
         <template #content>
           <p>Hello world!</p>
         </template>
@@ -12,6 +22,11 @@
 
 <script setup lang="ts">
 import BaseCard from "@/components/base/BaseCard.vue";
+import BaseButton from "@/components/base/BaseButton.vue";
+
+const startCreateTask = () => {
+  console.log("create task");
+};
 </script>
 
 <style scoped></style>
---
+++ src/components/base/BaseButton.vue
@@ -0,0 +1,20 @@
+<template>
+  <button
+    :type="type"
+    class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+  >
+    <slot />
+  </button>
+</template>
+
+<script setup lang="ts">
+interface Props {
+  type?: "button" | "submit";
+}
+
+withDefaults(defineProps<Props>(), {
+  type: "button",
+});
+</script>
+
+<style scoped></style>
